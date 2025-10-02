68419b5d46fc35bff83128f0e752a4f252786a43
Michelle123211 <M.stolova@seznam.cz>
2025-10-02 15:33:46 +0200

    Initial structure + versioning support
    - Endpoints in groups and separate extension methods
    - `Asp.Versioning.Http` + `Asp.Versioning.Mvc.ApiExplorer`
    - `ConfigureSwaggerOptions` - displaying different API versions

---
+++ EshopAPI/EshopAPI/Configuration/ConfigureSwaggerOptions.cs
@@ -0,0 +1,33 @@
+﻿using Asp.Versioning.ApiExplorer;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace EshopAPI.Configuration;
+
+/// <summary>
+/// A class handling configuration of Swagger documentation.
+/// Allows displaying different versions of API.
+/// </summary>
+public class ConfigureSwaggerOptions : IConfigureNamedOptions<SwaggerGenOptions> {
+
+	private readonly IApiVersionDescriptionProvider provider;
+
+	public ConfigureSwaggerOptions(IApiVersionDescriptionProvider provider) {
+		this.provider = provider;
+	}
+
+	public void Configure(string? name, SwaggerGenOptions options) {
+		foreach (var description in provider.ApiVersionDescriptions) {
+			var openApiInfo = new OpenApiInfo() {
+				Title = $"E-shop API v{description.ApiVersion}",
+				Version = description.ApiVersion.ToString()
+			};
+			options.SwaggerDoc(description.GroupName, openApiInfo);
+		}
+	}
+
+	public void Configure(SwaggerGenOptions options) {
+		Configure(options);
+	}
+}
--- EshopAPI/EshopAPI/Program.cs
+++ EshopAPI/EshopAPI/Program.cs
@@ -1,17 +1,52 @@
+using Asp.Versioning;
+using EshopAPI.Configuration;
+using EshopAPI.Endpoints.v1;
+using EshopAPI.Endpoints.v2;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Configure versioning
+builder.Services.AddApiVersioning(opts => {
+	opts.DefaultApiVersion = new ApiVersion(1);
+	opts.ReportApiVersions = true;
+	opts.ApiVersionReader = new UrlSegmentApiVersionReader();
+}).AddApiExplorer(opts => {
+	opts.GroupNameFormat = "'v'V";
+	opts.SubstituteApiVersionInUrl = true;
+});
+builder.Services.ConfigureOptions<ConfigureSwaggerOptions>();
+
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+
+// Distinguish between different API versions
+var apiVersionSet = app.NewApiVersionSet()
+	.HasApiVersion(new ApiVersion(1))
+	.HasApiVersion(new ApiVersion(2))
+	.ReportApiVersions()
+	.Build();
+var versionedGroup = app.MapGroup("/api/v{version:apiVersion}")
+	.WithApiVersionSet(apiVersionSet);
+
+
+// Register endpoints
+versionedGroup.RegisterProductsEndpointsV1();
+versionedGroup.RegisterProductsEndpointsV2();
+
+
+if (app.Environment.IsDevelopment()) {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options => {
+		var descriptions = app.DescribeApiVersions();
+		foreach (var desc in descriptions) {
+			options.SwaggerEndpoint($"/swagger/{desc.GroupName}/swagger.json", desc.GroupName.ToUpperInvariant());
+		}
+	});
 }
 
 app.UseHttpsRedirection();
