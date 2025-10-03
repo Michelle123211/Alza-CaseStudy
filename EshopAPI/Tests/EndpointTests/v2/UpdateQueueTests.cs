8086c6ded6d24becaaa840371e11fbc3a9e60956
Michelle123211 <M.stolova@seznam.cz>
2025-10-04 00:54:29 +0200

    Unit tests - update queue
    - Testing functionality of v2 asynchronous update API
    - `ProductUpdateQueueMock` - instead of the actual queue
    - All tests passing

---
+++ EshopAPI/Tests/EndpointTests/v2/UpdateQueueTests.cs
@@ -0,0 +1,116 @@
+﻿using EshopAPI.DTOs;
+using EshopAPI.Endpoints.v2;
+using EshopAPI.Entities;
+using EshopAPI.Infrastructure;
+using Microsoft.AspNetCore.Http.HttpResults;
+using System.Collections.Concurrent;
+using System.Data;
+using System.Xml.Linq;
+using Tests.Mocks;
+
+namespace Tests.EndpointTests.v2;
+
+/// <summary>
+/// A set of unit tests targeting "PUT /api/v2/products/{id}/quantity", 
+/// "PATCH /api/v2/products/{id}" and "GET /api/v2/products/{id}/status" endpoints.
+/// </summary>
+public class UpdateQueueTests {
+
+	private readonly ProductUpdateQueueMock queueMock = new();
+	private readonly ConcurrentDictionary<string, QueuedUpdateResult> statusDict = new();
+	private readonly ProductsRepositoryMock productsMock = new();
+
+	#region Whole product update
+	[Fact]
+	public async Task When_ProductUpdateRequested_Then_ReturnAccepted() {
+		// Arrange
+		queueMock.Reset();
+		int id = -123;
+		ProductUpdateDto productUpdateDto = new ProductUpdateDto() { Description = "Short description" };
+		// Act
+		var act = await ProductsEndpointsV2.EnqueueProductUpdate(queueMock, statusDict, productsMock, id, productUpdateDto);
+		// Assert
+		// ... type of result
+		Assert.NotNull(act);
+		Assert.IsType<AcceptedAtRoute>(act);
+		// ... status code
+		Assert.Equal(202, act.StatusCode);
+	}
+
+	[Fact]
+	public async Task When_ProductUpdateRequested_Then_Enqueue() {
+		// Arrange
+		queueMock.Reset();
+		int id = -123;
+		string newName = "Really cool name";
+		ProductUpdateDto productUpdateDto = new ProductUpdateDto() { Name = newName };
+		int enqueueCount = queueMock.MethodCalls.GetValueOrDefault(nameof(queueMock.EnqueueUpdateAsync), 0);
+		// Act
+		var act = await ProductsEndpointsV2.EnqueueProductUpdate(queueMock, statusDict, productsMock, id, productUpdateDto);
+		// Assert
+		Assert.Equal(1, queueMock.Count);
+		Assert.Equal(enqueueCount + 1, queueMock.MethodCalls.GetValueOrDefault(nameof(queueMock.EnqueueUpdateAsync), 0));
+		QueuedUpdate update = await queueMock.DequeueUpdateAsync(CancellationToken.None);
+		Assert.IsType<QueuedProductUpdate>(update);
+		QueuedProductUpdate productUpdate = (QueuedProductUpdate)update;
+		Assert.Equal(id, productUpdate.id);
+		Assert.Equivalent(productUpdateDto, productUpdate.productUpdate);
+
+	}
+	#endregion
+
+	#region Product quantity update
+	[Fact]
+	public async Task When_QuantityUpdateRequested_Then_ReturnAccepted() {
+		// Arrange
+		queueMock.Reset();
+		int id = -123;
+		int delta = 20;
+		// Act
+		var act = await ProductsEndpointsV2.EnqueueProductQuantityUpdate(queueMock, statusDict, productsMock, id, delta);
+		// Assert
+		// ... type of result
+		Assert.NotNull(act);
+		Assert.IsType<AcceptedAtRoute>(act);
+		// ... status code
+		Assert.Equal(202, act.StatusCode);
+	}
+
+	[Fact]
+	public async Task When_QuantityUpdateRequested_Then_Enqueue() {
+		// Arrange
+		queueMock.Reset();
+		int id = -123;
+		int delta = 20;
+		int enqueueCount = queueMock.MethodCalls.GetValueOrDefault(nameof(queueMock.EnqueueUpdateAsync), 0);
+		// Act
+		var act = await ProductsEndpointsV2.EnqueueProductQuantityUpdate(queueMock, statusDict, productsMock, id, delta);
+		// Assert
+		Assert.Equal(1, queueMock.Count);
+		Assert.Equal(enqueueCount + 1, queueMock.MethodCalls.GetValueOrDefault(nameof(queueMock.EnqueueUpdateAsync), 0));
+		QueuedUpdate update = await queueMock.DequeueUpdateAsync(CancellationToken.None);
+		Assert.IsType<QueuedQuantityUpdate>(update);
+		QueuedQuantityUpdate quantityUpdate = (QueuedQuantityUpdate)update;
+		Assert.Equal(id, quantityUpdate.id);
+		Assert.Equal(delta, quantityUpdate.quantityDelta);
+	}
+	#endregion
+
+	#region Status
+	[Fact]
+	public async Task When_IdDoesNotExist_Then_ReturnNotFound() {
+		// Arrange
+		Guid id = Guid.NewGuid();
+		// Act
+		var act = await ProductsEndpointsV2.GetStatus(statusDict, id.ToString());
+		// Assert
+		// ... type of result
+		Assert.IsType<NotFound<string>>(act.Result);
+		// ... status code
+		var result = act.Result as NotFound<string>;
+		Assert.NotNull(result);
+		Assert.Equal(404, result.StatusCode);
+	}
+	#endregion
+
+}
---
+++ EshopAPI/Tests/Mocks/ProductUpdateQueueMock.cs
@@ -0,0 +1,34 @@
+﻿using EshopAPI.Infrastructure;
+
+namespace Tests.Mocks;
+
+
+/// <summary>
+/// A class implementing <c>IProductUpdateQueue</c> which can be used to fake an actual queue pro update processing.
+/// Uses a simple queue and also manages method call counters which can be used to verify an interaction occurred.
+/// </summary>
+internal class ProductUpdateQueueMock : IProductUpdateQueue {
+
+	/// <summary>For each method, stores number of times it was called (to be able to verify whether an interaction with repository occurred).</summary>
+	public Dictionary<string, int> MethodCalls = new();
+
+	public int Count => queue.Count;
+
+	private Queue<QueuedUpdate> queue = new();
+
+	public void Reset() {
+		queue.Clear();
+		MethodCalls.Clear();
+	}
+
+	public Task EnqueueUpdateAsync(QueuedUpdate update) {
+		MethodCalls[nameof(EnqueueUpdateAsync)] = 1 + MethodCalls.GetValueOrDefault(nameof(EnqueueUpdateAsync), 0);
+		queue.Enqueue(update);
+		return Task.CompletedTask;
+	}
+
+	public Task<QueuedUpdate> DequeueUpdateAsync(CancellationToken stoppingToken) {
+		MethodCalls[nameof(DequeueUpdateAsync)] = 1 + MethodCalls.GetValueOrDefault(nameof(DequeueUpdateAsync), 0);
+		return Task.FromResult(queue.Dequeue());
+	}
+}
